baa3c30e4e7035199123dcb9a5d694e18c0ac34c
delanov <134703549+Vladislave232@users.noreply.github.com>
2024-05-28 22:52:45 +0300

    Add files via upload

--- JavaScript/longpoll/index.js
+++ JavaScript/longpoll/index.js
@@ -1,9 +1,10 @@
-const fastify = require('fastify')({ logger: false });
+const fastify = require('fastify')({ logger: false, connectionTimeout: 60000 });
+const formbody = require('@fastify/formbody');
 
-fastify.register(require('@fastify/formbody'));
+fastify.register(formbody);
 
 class LongPoll {
-  constructor(port = 13921, routePath = '/longpoll', tokens = {}) {
+  constructor(port = 13921, routePath = '/longpoll', tokens = []) {
     this.port = port;
     this.routePath = routePath;
     this.tokens = tokens;
@@ -13,84 +14,91 @@ class LongPoll {
       status: true,
       clientLost: false,
       messages: [],
+      res: null,
     }));
   }
 
   initialize(callback) {
-    fastify.get(this.routePath, (req, reply) => {
-      const isLocalhost = req.ip === '127.0.0.1' || req.ip === '::1';
-      if (!isLocalhost) {
-        reply.code(403).send('Access denied');
-        return;
+    fastify.get(this.routePath, async (req, reply) => {
+      if (!this.isLocalhost(req.ip)) {
+        return reply.code(403).send('Access denied');
       }
+
       const token = req.query.token;
-      if (!token || !getUser(this.tokens, token)) {
-        console.log(req.query);
+      const userNick = getUser(this.tokens, token);
+      if (!userNick) {
         return reply.code(403).send('Invalid Token');
       }
 
-      const userMessages = this.usersList.find(user => user.token === token);
-      if (userMessages && userMessages.messages.length > 0) {
-        reply.send(userMessages.messages);
-        userMessages.messages = [];
-      } else {
-        reply.send('failed');
-      }
-    });
-
-    fastify.post(this.routePath, (req, reply) => {
-      const isLocalhost = req.ip === '127.0.0.1' || req.ip === '::1';
-      if (!isLocalhost) {
-        reply.code(403).send('Access denied');
-        return;
-      }
-      const user = getUser(this.tokens, req.query.token) || false;
+      const user = this.usersList.find(user => user.nick === userNick);
       if (!user) {
-        return reply.code(403).send('Invalid Token');
-      }
-      if (!req.body.type) {
-        return reply.code(403).send('Invalid Type');
-      }
-
-      const currentUser = this.usersList.find(u => u.nick === user);
-      if (!currentUser) {
         return reply.code(403).send('Invalid User');
       }
 
-      if (req.body.type === 'checkstatus') {
-        currentUser.status = true;
-        currentUser.clientLost = false;
-      } else if (req.body.type === 'connect') {
-        this.handlerConnect(user);
-      } else {
-        this.messageHandler({ object: req.body.data, nick: user });
+      if (user.messages.length > 0) {
+        const messages = [...user.messages];
+        user.messages = [];
+        return reply.send(messages);
       }
-      reply.code(200).send();
+
+      reply.hijack();
+      user.res = reply.raw;
+
+      setTimeout(() => {
+        if (user.res) {
+          user.res.end(JSON.stringify([]));
+          user.res = null;
+        }
+      }, 30000); // Wait for a maximum of 30 seconds
     });
 
-    const checkStatus = () => {
-      this.usersList.forEach(user => {
-        if (!user.messages.includes('checkstatus')) {
-          user.messages.push('checkstatus');
-        }
-        if (user.status === false && !user.clientLost) {
-          this.handlerLost(user.nick);
-          user.clientLost = true;
-        }
-        user.status = false;
-      });
-      setTimeout(checkStatus, 2000);
-    };
-    checkStatus();
-
-    fastify.listen({ port: this.port, host: '0.0.0.0' }, (err, address) => {
-      if (typeof callback === 'function') {
-        callback();
+    fastify.post(this.routePath, async (req, reply) => {
+      if (!this.isLocalhost(req.ip)) {
+        return reply.code(403).send('Access denied');
       }
+
+      const token = req.query.token;
+      const userNick = getUser(this.tokens, token);
+      if (!userNick) {
+        return reply.code(400).send('Invalid Token');
+      }
+
+      const { type, data } = req.body;
+      if (!type) {
+        return reply.code(400).send('Invalid Type');
+      }
+
+      const user = this.usersList.find(u => u.nick === userNick);
+      if (!user) {
+        return reply.code(400).send('Invalid User');
+      }
+
+      switch (type) {
+        case 'checkstatus':
+          user.status = true;
+          user.clientLost = false;
+          break;
+        case 'connect':
+          this.handlerConnect(userNick);
+          break;
+        default:
+          try {
+            this.messageHandler({ object: data, nick: userNick });
+          } catch (error) {
+            console.error(error);
+          }
+      }
+      return reply.code(200).send();
+    });
+
+    this.checkStatus();
+
+    fastify.listen(this.port, '0.0.0.0', err => {
       if (err) {
         console.error(err);
         process.exit(1);
       }
+      if (callback) callback();
     });
   }
 
@@ -102,6 +110,11 @@ class LongPoll {
     const user = this.usersList.find(user => user.nick === name);
     if (user) {
       user.messages.push(longpollmessage);
+      if (user.res) {
+        user.res.end(JSON.stringify(user.messages));
+        user.messages = [];
+        user.res = null;
+      }
     }
   }
 
@@ -111,6 +124,25 @@ class LongPoll {
 
   onConnectClient(handler) {
     this.handlerConnect = handler;
+  }
+
+  checkStatus() {
+    setInterval(() => {
+      this.usersList.forEach(user => {
+        if (!user.messages.includes('checkstatus')) {
+          user.messages.push('checkstatus');
+        }
+        if (user.status === false && !user.clientLost) {
+          this.handlerLost(user.nick);
+          user.clientLost = true;
+        }
+        user.status = false;
+      });
+    }, 3000);
+  }
+
+  isLocalhost(ip) {
+    return ip === '127.0.0.1' || ip === '::1';
   }
 }
 
